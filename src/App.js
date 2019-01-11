8fa681b3fa16af2633bbaa65fdbae10e118d3e0a
Christopher Dennis <cdennis.aus@gmail.com>
2019-01-11 15:00:06 -0600

    Added new title with styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import './sass/main.css';
 class App extends Component {
   render() {
     return (
-      <div className="app">
+      <div id='app'>
+        <div id='app-title'>
+          <h1 id='app-title-main'>Twitch Viewer</h1>
+          <h1 id='app-title-shadow'>Twitch Viewer</h1>
+        </div>
         <Streamers />
       </div>
     );
